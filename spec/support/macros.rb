47434bb4623776506b7af5108909f5e1fc32a700
moralesalberto <alberto@moralitos.com>
2014-05-27 21:58:19 -0400

    adding the spec testing for sign in

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -2,6 +2,6 @@ def sign_in_user(the_user=nil)
   user = the_user || Fabricate(:user)
   visit sign_in_path
   fill_in 'email', :with => user.email
-  fill_in 'password', :with => "password"
+  fill_in 'password', :with => user.password
   click_button "Sign in"
 end
